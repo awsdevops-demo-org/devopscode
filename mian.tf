fb5cadaf02331631ce59d077998a93b464f81c0e
Ravi <Ravi@vytec.co>
2022-07-23 22:32:32 +0530

    AWSD-1012: added new files

---
+++ mian.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+  key_name = "oregon"
+  tags = {
+    Name = "Git client"
+  }
+}
